d9d62e3b22aa3abc0d342c0ae4b810ee9f862fba
gprat <gprat@Dhriti>
2016-08-20 12:56:00 -0400

    qeqe

---
+++ inheritance1/src/inheritance1/salmon.java
@@ -0,0 +1,20 @@
+package inheritance1;
+
+public class salmon extends tuna{
+int x;
+
+	int m;
+	salmon(int d,int e,int f,int g){
+	super(d,e,f);
+	
+	m=g;
+}
+	void show(int a){
+		
+	}
+	void show(int b,int c){
+		
+	}
+}
+
+
